44a913fbef5bb926cace24dcebd2858a8851a11a
UnfunLady <1986809887@qq.com>
2022-11-23 18:16:35 +0800

    save

--- src/main/java/com/example/webapp/bean/Depall.java
+++ src/main/java/com/example/webapp/bean/Depall.java
@@ -1,5 +1,6 @@
 package com.example.webapp.bean;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/example/webapp/bean/Dept.java
+++ src/main/java/com/example/webapp/bean/Dept.java
@@ -1,5 +1,7 @@
 package com.example.webapp.bean;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @TableName("dept")
 public class Dept {
+    @TableId(value="id",type= IdType.AUTO)
     private Integer id;
     private Integer deptno;
     private String deptname;
