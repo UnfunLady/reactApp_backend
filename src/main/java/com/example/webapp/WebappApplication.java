6df99b78ece1b57bee2d71955c86d7164a40109e
UnfunLady <1986809887@qq.com>
2023-01-09 17:40:50 +0800

    save

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -1,6 +1,6 @@
 package com.example.webapp;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
